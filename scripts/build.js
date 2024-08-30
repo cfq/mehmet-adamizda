562c4d25e08624537ef38d576f526db027389e26
Mehmet Duran <mehmet.duran@gmail.com>
2024-08-30 17:45:58 +0300

    de-webpackified

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,7 @@
 import { existsSync, mkdirSync, writeFileSync, cpSync } from 'fs';
 import nunjucks from 'nunjucks';
 import fetch from 'node-fetch';
+import * as sass from 'sass';
 
 async function fetchAdamiz() {
   const response = await fetch('https://4s7kcwiwv2.execute-api.us-east-1.amazonaws.com/dev/get-adamiz');
@@ -20,6 +21,12 @@ async function build() {
     mkdirSync('public');
   }
   writeFileSync('public/index.html', indexFile);
+
+  const compiledCSS = sass.compile("src/scss/main.scss", {style: "compressed"});
+  if(!existsSync('static/css')){
+    mkdirSync('static/css');
+  }
+  writeFileSync('static/css/styles.css', compiledCSS.css);
 
   cpSync('static', 'public/static', {recursive: true});
 }
